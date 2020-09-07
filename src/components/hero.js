6d8858e2fe5ebf297ccb1bbe753283b214ab744c
Archy de Berker <archy.deberker@gmail.com>
2020-09-07 18:31:28 -0400

    Fix link component by importing correctly

--- src/components/hero.js
+++ src/components/hero.js
@@ -4,11 +4,9 @@ import lightHero from "../assets/images/photo-1545529468-42764ef8c85f.jpeg";
 import styles from "./hero.module.css";
 // import Button from "../components/Button";
 import "../pages/mystyles.scss";
-import Link from "gatsby-image";
 
 const Hero = ({ title, subtitle, ...props }) => (
   <div className={styles.hero}>
-    {/* <img className={styles.heroImage} alt="hero-image" src={lightHero} /> */}
     <div className={styles.heroImage}>
       <h3 className={styles.heroHeadline}>{title}</h3>
       <p class="is-size-4">{subtitle}</p>
